7f5b837c15805f8f1df159cd458c7f65f9c067a4
zhangsan <zhangsan@sh.com>
2020-11-30 10:50:19 +0800

    vue import axios

---
+++ src/utils/request.js
@@ -0,0 +1,28 @@
+/*
+    基于 axios 封装的请求模板
+*/
+import axios from 'axios'
+
+// axios()
+// axios.get()
+// axios.post()
+
+// 创建一个axios 实例
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+// 请求拦截器
+
+// 响应拦截器
+
+// 导出请求方法
+export default request
+
+// 谁要使用谁就加载 request 模块
+// import request from 'request.js'
+// request.xxx
+// request({
+//   method: ,
+//   url: ''
+// })
