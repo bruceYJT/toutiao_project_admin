30bb9047ec66732f61cfb41d628b7d8da5fc4531
zhangsan <zhangsan@sh.com>
2020-11-30 16:00:38 +0800

    add layout|home_component(base

---
+++ src/views/layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="layout-container">
+    <div>顶部导航栏</div>
+    <div>侧边导航栏</div>
+    <!-- 路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less"></style>
