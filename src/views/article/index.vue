537af63c5f8845b47166bdbfb25a6f52cec33f9a
zhangsan <zhangsan@sh.com>
2020-12-03 16:17:20 +0800

    make publish_component(base

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -125,16 +125,6 @@ export default {
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
       articles: [],
       channels: [],
       articleStatus: [
