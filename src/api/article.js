37a1dd733d1bf315e4ae04e327c95e2248de19df
zhangsan <zhangsan@sh.com>
2020-12-03 16:47:43 +0800

    add image_collect

--- src/api/article.js
+++ src/api/article.js
@@ -29,3 +29,12 @@ export const deleteArticle = articleId => {
     url: `/mp/v1_0/articles/${articleId}`
   })
 }
+
+// 获取素材列表
+export const getImages = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
+  })
+}
--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -11,66 +11,17 @@
         <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
       </div>
       <div style="padding-bottom: 20px;">
-        <el-radio-group v-model="radio1" size="mini">
-          <el-radio-button label="全部"></el-radio-button>
-          <el-radio-button label="收藏"></el-radio-button>
+        <el-radio-group v-model="collect" size="mini" @change="onloadImage">
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
       </div>
       <!-- 素材列表 -->
       <el-row :gutter="10">
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+        <el-col :xs="12" :sm="6" :md="6" :lg="4" v-for='(img,index) in images' :key='index'>
           <el-image
             style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-            fit="cover"
-          ></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image
-            style="height: 100px"
-            src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            :src="img.url"
             fit="cover"
           ></el-image>
         </el-col>
@@ -81,20 +32,33 @@
 </template>
 
 <script>
+import { getImages } from '@/api/article.js'
 export default {
   name: 'ImageIndex',
   components: {},
   props: {},
   data () {
     return {
-      radio1: '全部'
+      radio1: '全部',
+      images: [],
+      collect: false
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.onloadImage()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    onloadImage () {
+      getImages({
+        collect: this.collect
+      }).then(res => {
+        this.images = res.data.data.results
+      })
+    }
+  }
 }
 </script>
 
