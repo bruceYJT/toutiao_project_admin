8529d0d995a0e412e54f2c1eeb7c73d324018299
zhangsan <zhangsan@sh.com>
2020-12-03 18:58:00 +0800

    add image_collect

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -11,22 +11,28 @@
         <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
       </div>
       <div style="padding-bottom: 20px;">
-        <el-radio-group v-model="collect" size="mini" @change="onloadImage">
+        <el-radio-group v-model="collect" size="mini" @change="onloadImage()" :disabled="loading">
           <el-radio-button :label="false">全部</el-radio-button>
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
-        <el-button class='alone' round size='mini' type='warning' @click='dialogUploadVisible = true'>上传素材</el-button>
+        <el-button class='alone' round size='mini' type='warning' @click='dialogUploadVisible = true' :disabled="loading">上传素材</el-button>
       </div>
       <!-- 素材列表 -->
       <el-row :gutter="10">
-        <el-col :xs="12" :sm="6" :md="6" :lg="4" v-for='(img,index) in images' :key='index'>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4" v-for='(img,index) in images' :key='index' style="position:relative">
           <el-image
             style="height: 100px"
             :src="img.url"
             fit="cover"
-          ></el-image>
-          <div class="img-item">
-
+          >
+          </el-image>
+          <div :class="{'img-item':(active_code != index), 'img_active_bg':(active_code == index)}" @mouseenter="active_code = index" @mouseleave="active_code = null">
+              <i
+              :class="{'el-icon-star-off':!img.is_collected, 'img-item-icon':true, 'deflault-icon':!img.is_collected, 'active-icon':img.is_collected, 'el-icon-star-on':img.is_collected}"
+              @click="oncollectit(img)"
+              :diabled = 'loading'
+              ></i>
+              <i class="el-icon-delete img-item-icon deflault-icon" :diabled = 'loading'></i>
           </div>
         </el-col>
       </el-row>
@@ -60,7 +66,7 @@
 </template>
 
 <script>
-import { getImages } from '@/api/article.js'
+import { getImages, collectImage } from '@/api/article.js'
 const user = JSON.parse(window.localStorage.getItem('user'))
 export default {
   name: 'ImageIndex',
@@ -78,7 +84,8 @@ export default {
       totalCount: null,
       pageSize: 20,
       page: 1,
-      loading: false
+      loading: true,
+      active_code: null
     }
   },
   computed: {},
@@ -89,6 +96,7 @@ export default {
   mounted () {},
   methods: {
     onloadImage (page = 1) {
+      this.loading = true
       getImages({
         page,
         collect: this.collect,
@@ -97,6 +105,7 @@ export default {
         const { results, total_count: totalCount } = res.data.data
         this.images = results
         this.totalCount = totalCount
+        this.loading = false
       })
     },
     onUploadSuccess () {
@@ -107,6 +116,18 @@ export default {
     },
     onCurrentChange () {
       this.onloadImage(this.page)
+    },
+    oncollectit (img) {
+      this.loading = true
+      collectImage(img.id, !img.is_collected).then(res => {
+        this.onloadImage(this.page)
+      }).catch(() => {
+        this.$message({
+          type: 'warning',
+          message: '收藏失败'
+        })
+      })
+      this.loading = false
     }
   }
 }
@@ -115,5 +136,45 @@ export default {
 <style scoped lang="less">
 .alone {
     margin-left: 20px;
+}
+.img-item {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    z-index: 999;
+    transition: all 0.3s;
+    .img-item-icon {
+        color: rgba(255, 255, 255,0);
+    }
+}
+.img_active_bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+    z-index: 999;
+    transition: all 0.3s;
+    background-color: rgba(200, 200, 200, 0.4);
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+    .img-item-icon {
+        float: left;
+        width: 30px;
+        height: 30px;
+        font-size: 30px;
+        line-height: 100px;
+        margin: 0 20px;
+        transition: all 0.3s;
+    }
+    .deflault-icon {
+        color:#fff;
+    }
+    .active-icon {
+        color: yellow;
+    }
 }
 </style>
